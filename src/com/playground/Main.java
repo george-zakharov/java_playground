a020c7ee470ff408d8aec5b4026d95b342f9f607
George Zakharov <george_zakharov@mail.ru>
2021-09-02 23:17:07 +0300

    Final method

--- src/com/playground/Main.java
+++ src/com/playground/Main.java
@@ -1,8 +1,15 @@
 package com.playground;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hi sweetheart");
+    public static void main(String[] args) throws Exception {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        byte[] digest = md.digest("abracadabra".getBytes(StandardCharsets.UTF_8));
+        for (byte b : digest) {
+            System.out.printf("%02x", b);
+        }
     }
 }
