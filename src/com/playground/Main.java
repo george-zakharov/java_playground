f4f97cc27f1100351ee36f9c42e7f127e32e8918
George Zakharov <george_zakharov@mail.ru>
2021-09-12 20:39:09 +0300

    Minor update

--- src/com/playground/Main.java
+++ src/com/playground/Main.java
@@ -4,8 +4,8 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 
 public class Main {
-
     public static void main(String[] args) throws Exception {
+        System.out.println("My beautiful program");
         MessageDigest md = MessageDigest.getInstance("MD5");
         byte[] digest = md.digest("abracadabra".getBytes(StandardCharsets.UTF_8));
         for (byte b : digest) {
